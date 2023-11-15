d0ad84565c54ac0ca9c9e1eb6e1c9e7f01f673c8
Kim Kokholm <kokholm@gmail.com>
2023-11-15 20:43:25 +0100

    New function added.

--- Source/com.sonnen.battery/drivers/sonnenbatterie/driver.ts
+++ Source/com.sonnen.battery/drivers/sonnenbatterie/driver.ts
@@ -6,14 +6,16 @@ class SonnenBatterieDriver extends Homey.Driver {
 
   /**
    * onInit is called when the driver is initialized.
-   */
-  async onInit() {
-    this.log('SonnenBatterieDriver has been initialized');
-
-    const setToC_card   = this.homey.flow.getActionCard("set-time-of-use");
-    const resetToC_card = this.homey.flow.getActionCard("reset-time-of-use");
-    const pauseToC_card = this.homey.flow.getActionCard("pause-time-of-use");
-    
+  */
+ async onInit() {
+   this.log('SonnenBatterieDriver has been initialized');
+   
+   const setToC_card   = this.homey.flow.getActionCard("set-time-of-use");
+   const setToCHours_card   = this.homey.flow.getActionCard("set-time-of-use-hours");
+   const resetToC_card = this.homey.flow.getActionCard("reset-time-of-use");
+   const pauseToC_card = this.homey.flow.getActionCard("pause-time-of-use");
+   const zeroPad = (num:any, places:any) => String(num).padStart(places, '0');
+   
     
     var batteryBaseUrl   = this.homey.settings.get("BatteryBaseUrl");
     var batteryAuthToken = this.homey.settings.get("BatteryAuthToken");
@@ -25,6 +27,52 @@ class SonnenBatterieDriver extends Homey.Driver {
       var maxPower  = args.MaxPower;
       
       this.log("args", timeStart, timeEnd, maxPower);
+
+      var options = {
+        method: 'put',
+        headers: {
+          'Auth-Token': `${batteryAuthToken}`,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      };
+
+      var body = {
+        "EM_ToU_Schedule": `[{\"start\":\"${timeStart}\",\"stop\":\"${timeEnd}\",\"threshold_p_max\":${maxPower}}]`
+      }
+  
+      // Act
+      axios.put(`${batteryBaseUrl}/api/v2/configurations`, body, options)
+        .then((response) => {
+          var batteryJson = response.data;
+          console.log("RESPONSE", batteryJson);
+          
+        })
+        .catch((error) => {
+          console.log("ERROR", error);
+        })
+        .finally(() => {
+          // always executed
+        });
+      
+
+
+    });
+
+    setToCHours_card.registerRunListener(async (args) => {
+      var timeStart = args.Start;
+      var hours     = args.Hours;
+      var maxPower  = args.MaxPower;
+
+      // Calculate end from timeStart and hours.
+      var timeStartHours    = +timeStart.split(":", 1)[0];
+      var timeStartMinutes  = +timeStart.split(":", 2)[1];
+      var timeEndHours = (timeStartHours + hours)%24; // Handle overflow.
+      var timeEndHoursFormatted = zeroPad(timeEndHours, 2);
+
+      var timeEnd = `${timeEndHoursFormatted}:${timeStartMinutes}`;
+
+      this.log("args", timeStart, hours, maxPower, timeEndHoursFormatted, "calculated endtime", timeEnd);
 
       var options = {
         method: 'put',
