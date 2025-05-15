10863ea0bf7da88e0665e2a3ccd4a5ad43dda091
mech78 <163742061+mech78@users.noreply.github.com>
2025-05-15 22:35:08 +0200

    add new sensor for household metering

---
+++ Source/com.sonnen.battery/drivers/sonnenbatterie_householdmeter/driver.ts
@@ -0,0 +1,45 @@
+import Homey from 'homey';
+import axios from 'axios';
+
+module.exports = class HouseholdMeterDriver extends Homey.Driver {
+
+  /**
+   * onInit is called when the driver is initialized.
+   */
+  async onInit() {
+    this.log('HouseholdMeterDriver has been initialized');
+  }
+
+  /**
+   * onPairListDevices is called when a user is adding a device and the 'list_devices' view is called.
+   * This should return an array with the data of devices that are available for pairing.
+   */
+  async onPairListDevices() {
+    try {
+      const response = await axios.get('https://find-my.sonnen-batterie.com/find');
+
+      if (response.data) {
+        this.log('results found', response.data);
+        const results = [];
+        for (const e of response.data) {
+          results.push({
+            name: e.info + "_meter",
+            data: {
+              id: e.device + "_meter",
+            },
+            store: {
+              lanip: e.lanip,
+            },
+          });
+        }
+
+        return results;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    return [];
+  }
+
+};
