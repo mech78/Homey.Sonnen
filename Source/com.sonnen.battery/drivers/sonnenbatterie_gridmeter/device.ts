123e2f06ccd66d5e2e7b996491f49eb17123142c
mech78 <163742061+mech78@users.noreply.github.com>
2025-06-08 09:24:01 +0200

    hide default capabilities, keeping them for energy tab, but avoid redundancies with custom ones for grid meter

--- Source/com.sonnen.battery/drivers/sonnenbatterie_gridmeter/device.ts
+++ Source/com.sonnen.battery/drivers/sonnenbatterie_gridmeter/device.ts
@@ -6,15 +6,17 @@ module.exports = class GridMeterDevice extends SonnenDevice {
     this.log("Received currentState: " + JSON.stringify(currentState, null, 2));
     this.log("Received statusJson:   " + JSON.stringify(statusJson, null, 2));
 
-    // FIXME: Using the (sub)default capability disallows overriding the icons, so use custom capabilities when the default capability is not required for the energy tab.
     this.setCapabilityValue('measure_power', -statusJson.GridFeedIn_W);
-    this.setCapabilityValue('measure_power.imported', +statusJson.GridFeedIn_W < 0 ? -1 * statusJson.GridFeedIn_W : 0);
-    this.setCapabilityValue('measure_power.exported', +statusJson.GridFeedIn_W > 0 ? +statusJson.GridFeedIn_W : 0);
-  
-    this.setCapabilityValue('meter_power.importedDaily', currentState.totalDailyGridConsumption_Wh / 1000);
-    this.setCapabilityValue('meter_power.exportedDaily', currentState.totalDailyGridFeedIn_Wh / 1000);
-    this.setCapabilityValue('meter_power.imported', currentState.totalGridConsumption_Wh / 1000);
-    this.setCapabilityValue('meter_power.exported', currentState.totalGridFeedIn_Wh / 1000);
+    this.setCapabilityValue('meter_power.import', currentState.totalGridConsumption_Wh / 1000);
+    this.setCapabilityValue('meter_power.export', currentState.totalGridFeedIn_Wh / 1000);
+
+    this.setCapabilityValue('grid_consumption_current_capability', +statusJson.GridFeedIn_W < 0 ? -1 * statusJson.GridFeedIn_W : 0);
+    this.setCapabilityValue('grid_consumption_daily_capability', currentState.totalDailyGridConsumption_Wh / 1000);
+    this.setCapabilityValue('grid_consumption_total_capability', currentState.totalGridConsumption_Wh / 1000);
+    
+    this.setCapabilityValue('grid_feed_in_current_capability', +statusJson.GridFeedIn_W > 0 ? +statusJson.GridFeedIn_W : 0);
+    this.setCapabilityValue('grid_feed_in_daily_capability', currentState.totalDailyGridFeedIn_Wh / 1000);
+    this.setCapabilityValue('grid_feed_in_total_capability', currentState.totalGridFeedIn_Wh / 1000);
   }
 
   async onInit() {
