ead985fc4383803508449332a4b2b9b28b295440
mech78 <163742061+mech78@users.noreply.github.com>
2025-06-08 07:12:00 +0200

    added more grid capabilities and tried to change icons, but icons don't seem to override for defaul subcapabilities

--- Source/com.sonnen.battery/drivers/sonnenbatterie_gridmeter/device.ts
+++ Source/com.sonnen.battery/drivers/sonnenbatterie_gridmeter/device.ts
@@ -7,7 +7,9 @@ module.exports = class GridMeterDevice extends SonnenDevice {
     this.log("Received statusJson:   " + JSON.stringify(statusJson, null, 2));
 
     this.setCapabilityValue('measure_power', -statusJson.GridFeedIn_W);
-    //this.setCapabilityValue('meter_power', currentState.totalConsumption_Wh / 1000);
+    this.setCapabilityValue('measure_power.imported', +statusJson.GridFeedIn_W < 0 ? -1 * statusJson.GridFeedIn_W : 0);
+    this.setCapabilityValue('measure_power.exported', +statusJson.GridFeedIn_W > 0 ? +statusJson.GridFeedIn_W : 0);
+  
     this.setCapabilityValue('meter_power.importedDaily', currentState.totalDailyGridConsumption_Wh / 1000);
     this.setCapabilityValue('meter_power.exportedDaily', currentState.totalDailyGridFeedIn_Wh / 1000);
     this.setCapabilityValue('meter_power.imported', currentState.totalGridConsumption_Wh / 1000);
