6273b7e561df1013354bb0b5a5525119ddbcafc1
mech78 <163742061+mech78@users.noreply.github.com>
2025-06-22 17:00:39 +0200

    reformat, use braces

--- Source/com.sonnen.battery/drivers/sonnenbatterie_gridmeter/driver.ts
+++ Source/com.sonnen.battery/drivers/sonnenbatterie_gridmeter/driver.ts
@@ -7,25 +7,26 @@ module.exports = class GridMeterDriver extends SonnenDriver {
     this.deviceId = "gridMeter";
     super.onInit();
 
-    const toGridTrigger         = this.homey.flow.getConditionCard("deliver-to-grid");
-    const fromGridTrigger       = this.homey.flow.getConditionCard("consumption-from-grid");
+    const toGridTrigger = this.homey.flow.getConditionCard("deliver-to-grid");
+    const fromGridTrigger = this.homey.flow.getConditionCard("consumption-from-grid");
 
     toGridTrigger.registerRunListener(async (args) => {
       var toGridValue = +this.getDevices()[0].getCapabilityValue("grid_feed_in_current_capability");
       this.log("TRIGGER", "toGrid", toGridValue, "arg", args.Power, "VALID", toGridValue > 0);
-      
-      if (toGridValue > 0)
-        return false; // not current feeding  grid
 
-        return (toGridValue < args.Power);
+      if (toGridValue > 0) {
+        return false; // not current feeding  grid
+      }
+      return (toGridValue < args.Power);
     });
 
-    fromGridTrigger.registerRunListener(async (args) => {      
-      var fromGridValue = +this.getDevices()[0].getCapabilityValue("grid_consumption_current_capability");      
+    fromGridTrigger.registerRunListener(async (args) => {
+      var fromGridValue = +this.getDevices()[0].getCapabilityValue("grid_consumption_current_capability");
       this.log("TRIGGER", "fromGrid", fromGridValue, "arg", args.Power, "VALID", fromGridValue < 0);
 
-      if (fromGridValue < 0)
+      if (fromGridValue < 0) {
         return false; // not current consuming from grid
+      }
 
       return (fromGridValue > args.Power);
     });
