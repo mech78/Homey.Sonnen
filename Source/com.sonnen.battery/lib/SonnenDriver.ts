269ef226fc7af3f7534952239a8388eec0102c2e
mech78 <163742061+mech78@users.noreply.github.com>
2025-07-27 20:12:38 +0200

    stricter ts checks

--- Source/com.sonnen.battery/lib/SonnenDriver.ts
+++ Source/com.sonnen.battery/lib/SonnenDriver.ts
@@ -9,7 +9,7 @@ export abstract class SonnenDriver extends Homey.Driver {
   /**
    * onInit is called when the driver is initialized.
    */
-  async onInit() {
+  override async onInit(): Promise<void> {
     super.onInit();
     this.log(this.constructor.name + ' has been initialized for device: ' + this.deviceName + ' with ID: ' + this.deviceId);
   }
@@ -18,7 +18,7 @@ export abstract class SonnenDriver extends Homey.Driver {
     * onPairListDevices is called when a user is adding a device and the 'list_devices' view is called.
     * This should return an array with the data of devices that are available for pairing.
     */
-  async onPairListDevices() {
+  override async onPairListDevices(): Promise<Array<{ name: string; data: { id: string }; store: { lanip: string } }>> {
     try {
       const response = await axios.get('https://find-my.sonnen-batterie.com/find');
 
