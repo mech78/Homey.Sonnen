d65e59f780d10e794fa7d73d4f55935710e47534
Kim Kokholm <kokholm@gmail.com>
2025-11-10 20:47:56 +0100

    Possible correct fix.

--- Source/com.sonnen.battery/drivers/sonnenbatterie/driver.ts
+++ Source/com.sonnen.battery/drivers/sonnenbatterie/driver.ts
@@ -68,13 +68,11 @@ module.exports = class SonnenBatterieDriver extends SonnenDriver {
   };
 
   private async handleSetTimeOfUseByStartTimeAndHours(args: { device: BatteryDevice, start: string, hours: number, max_power: number }): Promise<void> {
-    const timeStart = args.start.trim();
-    const hours = args.hours;
-    const maxPower = args.max_power;
+    const argTimeStart = args.start.trim();
 
     // Calculate end from timeStart and hours.
-    const timeStartHours    = Number(args.start.split(":")[0]);
-    const timeStartMinutes  = Number(args.start.split(":")[1]);
+    const timeStartHours    = Number(argTimeStart.split(":")[0]);
+    const timeStartMinutes  = Number(argTimeStart.split(":")[1]);
     const timeEndHours = (timeStartHours + args.hours) % 24; // Handle overflow.
 
     const timeStartHoursFormatted = this.zeroPad(timeStartHours, 2);
