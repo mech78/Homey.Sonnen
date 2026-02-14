c1d46fc9dcb25621b57e547339400d5869ecba71
mech78 <163742061+mech78@users.noreply.github.com>
2026-02-14 18:17:09 +0100

    improve test support

--- Source/com.sonnen.battery/jest.config.js
+++ Source/com.sonnen.battery/jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  displayName: 'Jest Tests',
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', {
+      isolatedModules: true
+    }],
   },
   collectCoverageFrom: [
     'domain/**/*.ts',
